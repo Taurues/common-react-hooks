e1cd3a9da00effea354dd348ac825a9df22c253b
张焱如 <zhangyanru@wshifu.com>
2023-08-18 15:34:33 +0800

    feat: .irc文件修改

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
-  outputPath: 'docs-dist',
+  outputPath: './docs-dist',
   themeConfig: {
     name: 'my-hooks',
   },
+  history: {
+    type: 'hash',
+  },
+  base: '/common-react-hooks/',
+  publicPath: '/common-react-hooks/',
   extraBabelPlugins: [
     [
       'babel-plugin-import',
